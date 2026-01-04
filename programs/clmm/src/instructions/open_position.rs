8f4f0a5eb0b789f3cb5e927dbc242edb04357ed6
Anand Chaudhary <achaudharyskn@gmail.com>
2026-01-04 17:36:39 +0530

    open position accounts implemented with TickArray

--- programs/clmm/src/instructions/open_position.rs
+++ programs/clmm/src/instructions/open_position.rs
@@ -1,9 +1,10 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token_interface::TokenAccount;
+use anchor_spl::{ token_interface::{Mint, TokenAccount, TokenInterface}};
+use crate::states::{Pool, Position, TickArray};
 
-use crate::states::{Pool, Position};
+
 #[derive(Accounts)]
-#[instruction(lower_tick:i32,uppar_tick:i32,liquidity_amount:u128.tick_array_lower_start_index:i32,tick_array_uppar_start_index:i32)]
+#[instruction(lower_tick:i32,uppar_tick:i32,liquidity_amount:u128,tick_array_lower_start_index:i32,tick_array_uppar_start_index:i32)]
 pub struct OpenPosition<'info>{
     #[account(mut)]
     pub payer : Signer<'info>,
@@ -31,7 +32,47 @@ pub struct OpenPosition<'info>{
         ],
         bump
     )]
-    pub lower_tick_array : Account<'info,TickArray>
+    pub lower_tick_array : Account<'info,TickArray>,
 
+    #[account(
+        init_if_needed,
+        payer = payer ,
+        space = TickArray::SPACE,
+        seeds = [
+            b"tick_array",
+            pool.key().as_ref(),
+            &tick_array_uppar_start_index.to_le_bytes()
+        ],
+        bump
+    )]
+    pub uppar_tick_array : Account<'info,TickArray>,
 
+    #[account(
+        init_if_needed,
+        payer = payer,
+        space = Position::INIT_SPACE,
+        seeds = [
+            b"position",
+            payer.key().as_ref(),
+            pool.key().as_ref(),
+            &lower_tick.to_le_bytes(),
+            &uppar_tick.to_le_bytes()
+        ],
+        bump
+    )]
+    pub position : Account<'info, Position>,
+    
+    pub token_mint_0 : InterfaceAccount<'info,Mint>,
+    pub token_mint_1 : InterfaceAccount<'info,Mint>,
+
+    #[account(mut)]
+    pub user_token_0: InterfaceAccount<'info, TokenAccount>,
+    #[account(mut)]
+    pub user_token_1 : InterfaceAccount<'info,TokenAccount>,
+    #[account(mut)]
+    pub pool_token_0 : InterfaceAccount<'info,TokenAccount>,
+    #[account(mut)]
+    pub pool_token_1 : InterfaceAccount<'info,TokenAccount>,
+    pub system_program : Program<'info,System>,
+    pub token_program : Interface<'info,TokenInterface>,
 }
